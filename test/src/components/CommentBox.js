d79263fe83539ec3f7993446516c39c77b995aab
github username <github email address>
2018-09-22 15:21:53 -0400

    auth HOC done

--- test/src/components/CommentBox.js
+++ test/src/components/CommentBox.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/index';
+import requireAuth from './requireAuth'
 
 class CommentBox extends Component {
   state = { comment: '' }
-
-  componentDidMount() {
-    this.shouldNavigateAway()
-  }
-
-  componentDidUpdate() {
-    this.shouldNavigateAway()
-  }
-
-  shouldNavigateAway() {
-    if (!this.props.auth) {
-      console.log('NO')
-    }
-  }
 
   handleChange = (event) => {
     this.setState({ comment: event.target.value })
@@ -45,8 +32,4 @@ class CommentBox extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return { auth: state.auth };
-}
-
-export default connect(mapStateToProps, actions)(CommentBox);
+export default connect(null, actions)(requireAuth(CommentBox));
